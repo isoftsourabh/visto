0de2c75a6931a922237af0cc6a5bf877bca56168
Sourabh <isoftsourabhb@gmail.com>
2025-01-31 17:08:42 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require('cors')
+require("./config")
+
+
+app.use(express.json())
+app.use(cors({
+    origin: true,
+    methods:["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+})
+);
+const userRouter = require('./routes/userRoute');
+app.use(userRouter)
+
+
+app.listen(3000, ()=>{console.log("Server up and running on port 3000!")});
